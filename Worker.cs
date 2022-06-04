3c0e4506b074f024fbdb074bbb34cbe9843624e4
Luke Bakken <luke@bakken.io>
2022-06-04 08:10:18 -0700

    Fill in RabbitMQ client code

--- Worker.cs
+++ Worker.cs
@@ -4,21 +4,57 @@
 
 namespace rabbitmq_backgroundservice;
 
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
 public class Worker : BackgroundService
 {
+    private readonly TimeSpan _stoppingCheckInterval = TimeSpan.FromSeconds(5);
     private readonly ILogger<Worker> _logger;
+    private readonly IConnection _connection;
+    private readonly IModel _channel;
+    private readonly EventingBasicConsumer _consumer;
+    private readonly string _consumerTag;
 
     public Worker(ILogger<Worker> logger)
     {
         _logger = logger;
+
+        var factory = new ConnectionFactory {
+            HostName = "localhost",
+            UserName = "guest",
+            Password = "guest"
+        };
+
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
+
+        _consumer = new EventingBasicConsumer(_channel);
+        _consumer.Received += ReceivedHandler;
+
+        _consumerTag = _channel.BasicConsume("MyQueue", false, _consumer);
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        using (_connection)
+        using (_channel)
         {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(_stoppingCheckInterval, stoppingToken);
+            }
+
+            _logger.LogInformation("Worker STOPPING at: {time}", DateTimeOffset.Now);
+            _channel.BasicCancel(_consumerTag);
         }
+    }
+
+    private void ReceivedHandler(object? sender, BasicDeliverEventArgs ea)
+    {
+        var tag = ea.DeliveryTag;
+        _logger.LogInformation("Received message. tag: {tag}  at: {time}", tag, DateTimeOffset.Now);
+        _channel.BasicAck(tag, false);
     }
 }
